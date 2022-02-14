ea802744a23f20569c6cacc4bc07dae48db1d24d
Jon Friesen <jfriesen@digitalocean.com>
2022-02-14 13:39:01 -0800

    adds php helloworld example

---
+++ README.md
@@ -0,0 +1,4 @@
+# PHP Hello world sample function
+
+# Usage
+Accessing the function with the query parameter `?name=Alice` will greet Alice. No query parameters will greet strangers.
---
+++ packages/sample/hello/index.php
@@ -0,0 +1,13 @@
+<?php
+ 
+function main(array $args) : array
+{
+    $name = $args["name"] ?? "stranger";
+    
+    $greeting = "Hello {$name}!";
+    echo $greeting;
+ 
+    return [
+        'body' => $greeting,
+    ];
+}
